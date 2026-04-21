66a86bcc25b11c95bdf497574cdb21cb4268e5fe
Giovanna Fernandes <giovanna.fernandes2009@gmail.com>
2026-04-21 19:27:33 -0300

    Create ExercicioTemperatura.java

---
+++ Exercícios realizados/ExercicioTemperatura.java
@@ -0,0 +1,23 @@
+// Exercício 01 — Temperatura: Leia a temperatura atual (double). Se estiver entre 15°C e 25°C, mostre "Temperatura dentro dos limites seguros". 
+// Caso contrário, mostre "Atencao! Temperatura fora dos limites seguros".
+
+import java.util.Scanner;
+
+public class exercicio {
+    public static void main(String[] args){
+        Scanner leitura = new Scanner(System.in);
+        double temperatura;
+
+        System.out.println("***VERIFICADOR DE TEMPERATURA***");
+        System.out.println("Digite a temperatura: ");
+        temperatura = leitura.nextDouble();
+
+        if (temperatura >= 15 && temperatura <= 25) {
+            System.out.println("Temperatura dentro dos limites seguros");
+        } else {
+            System.out.println("Atencão! Temperatura fora dos limites seguros");
+        }
+
+        leitura.close();
+    }
+}
